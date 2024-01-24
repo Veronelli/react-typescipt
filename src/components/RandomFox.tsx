feaedc6dc67cb7e023012d33566b6bb74275679f
Veronelli <facu2000veronelli@outlook.com>
2024-01-24 09:25:21 -0300

    add type

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,11 +1,21 @@
-"use client";
-
+import { useState } from "react";
 type TProps = {
-    image: string
-}
+  image: string;
+};
+const RANDOM_IMAGE_NUMBER =()=> Math.floor(Math.random() * 123) + 1;
 
-export const RandomFox = ({image}:TProps): JSX.Element => {
-    const RANDOM_IMAGE_NUMBER = Math.floor(Math.random() * 123) + 1;
-    const imageURL = `${image}${RANDOM_IMAGE_NUMBER}.jpg`;
-  return <img src={imageURL} />;
+export const RandomFox = ({ image }: TProps): JSX.Element => {
+  const [images, setImages] = useState<string[]>([
+    `${image}${RANDOM_IMAGE_NUMBER()}.jpg`,
+    `${image}${RANDOM_IMAGE_NUMBER()}.jpg`,
+    `${image}${RANDOM_IMAGE_NUMBER()}.jpg`,
+    `${image}${RANDOM_IMAGE_NUMBER()}.jpg`,
+  ]);
+  return (
+    <>
+      {images.map((image, index) => (
+        <img key={index} src={image} alt="Random fox" />
+      ))}
+    </>
+  );
 };
